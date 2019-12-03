9f4e98b5947142f94278eab4442e718935f6ffe2
Maiamba <mayambalive1999@outlook.com>
2019-12-03 14:32:40 +0100

    all

--- SmartInfo/SmartInfo.Android/MessageAlert.cs
+++ SmartInfo/SmartInfo.Android/MessageAlert.cs
@@ -24,7 +24,7 @@ namespace SmartInfo.Droid
 
         public void ShortAlert(string message)
         {
-            Toast.MakeText(Application.Context, message, ToastLength.Short).Show();
+            Toast.MakeText(Application.Context, message, ToastLength.Long).Show();
         }
     }
 }
